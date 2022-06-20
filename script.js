da0039a38b4772b117d3579afbb4e842f6c0d063
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-06-21 01:24:22 +0430

    Create a simple table through AngularJS

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+  $scope.names = [
+    { name: "Sepideh", city: "Mashhad" },
+    { name: "Zeinab", city: "Tehran" },
+    { name: "Sara", city: "Mashhad" },
+    { name: "Faranak", city: "Tehran" },
+    { name: "Maryam", city: "Shiraz" },
+    { name: "Ameneh", city: "Abadan" },
+    { name: "Zahra", city: "Birjand" },
+    { name: "Saeed", city: "Rasht" },
+    { name: "Amir", city: "Ghazvin" },
+    { name: "Yalda", city: "Hamedan" },
+  ];
+});
